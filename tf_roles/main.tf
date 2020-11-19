4b43e7b6c4664aa90fb7877e89584a336bc92dd6
Pradeepkumar <31471389+TechyCloud@users.noreply.github.com>
2020-11-19 15:28:06 +0530

    Update main.tf

--- tf_roles/main.tf
+++ tf_roles/main.tf
@@ -3,7 +3,7 @@
 module "module-iam-roles" {
 source = "../modules/module-iam-roles/"
 
-role-name = "Lambda-PradeepRole"
+role-name = "Lambda-Role"
 service-name = "lambda.amazonaws.com"
 policy-arn = "arn:aws:iam::aws:policy/AWSLambdaFullAccess"
 }
